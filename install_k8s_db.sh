52756fd606131b3220fad9bf8de494a7008c56a8
robotism <fan@gankcode.com>
2024-06-08 20:01:28 +0800

    update

--- install_k8s_db.sh
+++ install_k8s_db.sh
@@ -87,8 +87,8 @@ helm upgrade --install elasticsearch bitnami/elasticsearch \
 
 ## install victoriametrics
 # https://github.com/VictoriaMetrics/helm-charts
-helm repo add victoriametrics ${GHPROXY}https://victoriametrics.github.io/helm-charts/
-helm upgrade --install victoria-metrics-cluster vm/victoria-metrics-cluster \
+helm repo add victoriametrics https://victoriametrics.github.io/helm-charts/
+helm upgrade --install victoria-metrics-cluster victoriametrics/victoria-metrics-cluster \
   --set persistentVolume.storageClass=${storage_class} \
   --set vmselect.fullnameOverride=victoria-metrics-cluster-vmselect \
   --set vminsert.fullnameOverride=victoria-metrics-cluster-vminsert \
