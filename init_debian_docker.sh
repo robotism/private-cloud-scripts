0a7f75000398e7559856dd4e9aeee914e06cb50e
robotism <fan@gankcode.com>
2024-06-08 16:42:41 +0800

    update

--- init_debian_docker.sh
+++ init_debian_docker.sh
@@ -155,26 +155,21 @@ fi
 init_containerd_config=`getarg init_docker_config $@`
 if [ "$init_containerd_config" != "false" ]; then
 export PS4='\[\e[35m\]+ $(basename $0):${FUNCNAME}:$LINENO: \[\e[0m\]'
- 
 config_file="/etc/containerd/config.toml"
 config_path='/etc/containerd/certs.d'
- 
 if [ ! -f "${config_file}" ];then
     [ ! -d "${config_file%/*}" ] && mkdir -p ${config_file%/*}
     lineno="$(containerd config default | grep -n -A 1 -P '(?<=\[plugins.")io.containerd.grpc.v1.cri(?=".registry])'|tail -1)"
     lineno=${lineno/-*}
     containerd config default | sed -e "${lineno}s@config.*@config_path = \"${config_path}\"@" |sed '/SystemdCgroup/s/false/true/' > $config_file
 fi
- 
 [ ! -d "${config_path}" ] && mkdir -p ${config_path}
 params="${@:-registry.k8s.io:k8s.m.daocloud.io docker.io:docker.m.daocloud.io gcr.io:gcr.m.daocloud.io k8s.gcr.io:k8s.m.daocloud.io quay.io:quay.m.daocloud.io}"
- 
 function content(){
     printf 'server = "https://%s"\n'  "${registry}"
     printf '[host."https://%s"]\n' "${proxy_server}"
     printf '  capabilities = ["pull", "resolve"]'
 }
- 
 for param in ${params}
 do
     registry="${param/:*/}"
@@ -183,11 +178,6 @@ do
     [ ! -d "$hosts_path" ] && mkdir -p ${hosts_path}
     content > $hosts_path/hosts.toml
 done
-
-sudo tee $config_file -a <<-'EOF'
-  [plugins.cri.registry.mirrors."docker.io"]
-    endpoint = ["https://fogjl973.mirror.aliyuncs.com", "https://registry-1.docker.io"]
-EOF
 systemctl restart containerd
 fi
 
