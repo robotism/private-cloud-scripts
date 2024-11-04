066a18a1a59e66f6c248b550ed23c6585f406ba2
robotism <fan@gankcode.com>
2024-11-05 00:53:05 +0800

    update

--- install_docker_frps.sh
+++ install_docker_frps.sh
@@ -82,7 +82,6 @@ docker run -dit \
   --privileged \
   --network=${network:-bridge} \
   -p ${port_bind}:${port_bind} \
-  -v ${datadir}/${FRPS_CONFIG}:/etc/frp/${FRPS_CONFIG} \
   -l traefik.enable=true \
   -l traefik.http.routers.${CONTAINER_NAME}-http.rule=${http_route_rule} \
   -l traefik.http.routers.${CONTAINER_NAME}-http.tls=${tls} \
@@ -103,9 +102,9 @@ docker run -dit \
   -l traefik.http.routers.${CONTAINER_NAME}-ui.service=${CONTAINER_NAME}-ui@docker \
   -l traefik.http.services.${CONTAINER_NAME}-ui.loadbalancer.server.port=${port_ui} \
   --name=${CONTAINER_NAME} \
-  --entrypoint=frps \
+  --entrypoint="" \
   docker.io/snowdreamtech/frps:latest \
-  -c /etc/frp/${FRPS_CONFIG}
+  frps -c /etc/frp/${FRPS_CONFIG}
 
 docker ps -a | grep ${CONTAINER_NAME} 2>/dev/null
 docker logs -n 1000 ${CONTAINER_NAME} 2>/dev/null
